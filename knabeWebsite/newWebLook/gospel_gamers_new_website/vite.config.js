21651c8c9655b9d121168e7eba604491ef27ee4a
gospel_gamers <107871045+gospel-gamers@users.noreply.github.com>
2023-09-05 16:25:11 -0400

    comment out subpath location

--- knabeWebsite/newWebLook/gospel_gamers_new_website/vite.config.js
+++ knabeWebsite/newWebLook/gospel_gamers_new_website/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/knabeWebsite/newWebLook/gospel_gamers_new_website/',
+  // base: '/knabeWebsite/newWebLook/gospel_gamers_new_website/',
 })
