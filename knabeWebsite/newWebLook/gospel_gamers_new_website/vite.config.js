0f5b669eee9e399f21a11908242ec70e60c7820c
gospel_gamers <107871045+gospel-gamers@users.noreply.github.com>
2023-09-05 15:41:23 -0400

    Configure GitHub Pages for Subdirectory Deployment
    for deployment on GitHub pages

--- knabeWebsite/newWebLook/gospel_gamers_new_website/vite.config.js
+++ knabeWebsite/newWebLook/gospel_gamers_new_website/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/knabeWebsite/newWebLook/gospel_gamers_new_website/',
 })
